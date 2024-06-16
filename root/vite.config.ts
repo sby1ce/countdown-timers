c3539e69e85f66c301d5a7b6efe4e802534d082d
SomeBodY1CE <someonecallsyou@gmail.com>
2024-06-16 20:53:30 +0300

    feat(root): base path + favicon

---
+++ root/vite.config.ts
@@ -0,0 +1,14 @@
+/*
+Copyright 2024 sby1ce
+
+SPDX-License-Identifier: CC0-1.0
+*/
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    target: "esnext",
+  },
+  base: "/countdown-timers",
+});
