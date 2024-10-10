34032aaaf7280f3df5487f1091297a6fc5ac9029
SomeBodY1CE <someonecallsyou@gmail.com>
2024-10-10 22:21:10 +0300

    feat(ffi): its done

--- countdown-rs/cd-native/src/lib.rs
+++ countdown-rs/cd-native/src/lib.rs
@@ -4,33 +4,16 @@ Copyright 2024 sby1ce
 SPDX-License-Identifier: AGPL-3.0-or-later
 */
 
+use chrono::Utc;
 use core::{mem::ManuallyDrop, slice};
-
-fn work(origins: &[i64]) -> Vec<String> {
-    origins.iter().map(ToString::to_string).collect()
-}
-
-/// Encodes `String`s into UTF-16 and then marks them for manual drop
-///
-/// Returns a vector of `usize`s,
-/// where every element with an even index is an array pointer
-/// and every element with an odd index is a length of the array of the previous pointer
-fn convert_vecs(data: Vec<String>) -> Vec<usize> {
-    data.into_iter()
-        .flat_map(|s: String| -> [usize; 2] {
-            let manual: ManuallyDrop<Vec<u16>> = ManuallyDrop::new(s.encode_utf16().collect());
-            let ptr: *const u16 = manual.as_ptr();
-            let length: usize = manual.len();
-            [ptr as usize, length]
-        })
-        .collect()
-}
 
 /// Reconstruct a `slice` that was initialized in JavaScript
 unsafe fn reconstruct_slice<T>(array_ptr: &*const T, length: libc::size_t) -> &[T] {
     assert!(!array_ptr.is_null());
     assert!(array_ptr.is_aligned());
     assert!(length < usize::MAX / 4);
+    // JavaScript owns the data but core consumes the iterator
+    // surely the compiler will optimize away the copying
     let array: &[T] = unsafe { slice::from_raw_parts(*array_ptr, length) };
     array
 }
@@ -44,32 +27,22 @@ unsafe fn reconstruct_vec<T>(array_ptr: *mut T, length: libc::size_t) -> Vec<T> 
     vector
 }
 
-/// # Safety
-///
-/// To ensure safety, only pass `TypedArray`s directly and their valid length.
-/// Also do not mutate the reconstructed `ArrayBuffer` in JS
-///
-/// # Panics
-///
-/// Panics if passed in array pointer is null or not aligned
-#[no_mangle]
-pub unsafe extern "C" fn work_nat(array_ptr: *const i64, length: libc::size_t) -> Box<[usize; 2]> {
-    let origins: &[i64] = unsafe { reconstruct_slice(&array_ptr, length) };
-    let worked: Vec<String> = work(origins);
-    let pointers: ManuallyDrop<Vec<usize>> = ManuallyDrop::new(convert_vecs(worked));
-    let ptr: *const usize = pointers.as_ptr();
-    let length: usize = pointers.len();
-
-    Box::new([ptr as usize, length])
-}
-
 /// Bun FFI doesn't support tuples or structs yet
 ///
 /// Constructing owning `Vec` from raw parts is impossible,
 /// because JavaScript owns the data (even though it's being consumed here) and uses own allocator
 ///
-/// Outputs a `Vec<u16>`.
+/// Outputs a `Vec<Vec<u16>>` as a `BigUint64Array` 
+/// where every even element is a pointer to inner `Vec` 
+/// and every odd element is the length of the `Vec` previous pointer points to. 
+/// 
+/// I guarantee the array length will be even. 
+/// 
+/// Drop it using `drop_pointers`.
+/// 
 /// Returns a pointer to `Vec` raw parts to pass to `toArrayBuffer` in JavaScript
+/// 
+/// TODO make it putput `Vec<Vec<Vec<u16>>`
 ///
 /// # Safety
 ///
@@ -82,23 +55,35 @@ pub unsafe extern "C" fn work_nat(array_ptr: *const i64, length: libc::size_t) -
 #[no_mangle]
 pub unsafe extern "C" fn update_timers(
     array_ptr: *const i64,
-    len: libc::size_t,
+    length: libc::size_t,
 ) -> Box<[usize; 2]> {
-    assert!(!array_ptr.is_null());
-    assert!(array_ptr.is_aligned());
-    assert!(len < usize::MAX / 4);
-    // JavaScript owns the data but core consumes the iterator
-    // surely the compiler will optimize away the copying
-    let origins: &[i64] = unsafe { slice::from_raw_parts(array_ptr, len) };
-    println!("{origins:?}");
+    let origins: &[i64] = unsafe { reconstruct_slice(&array_ptr, length) };
+
+    // Using `chrono` instead of `std::time` because that one uses u128
+    let now: i64 = Utc::now().timestamp_millis();
+    let renders: Vec<[String; 1]> = cd_core::update_timers_(now, origins.to_owned());
 
     // The docs don't specify what happens to the memory of the pointer, length and capacity
     // when a `Vec` is marked for manual dropping
     // presumably this means they get cleaned up
-    // "0" and "1" in UTF-16
-    let vector: ManuallyDrop<Vec<u16>> = ManuallyDrop::new(vec![48, 49]);
-    let out_ptr: *const u16 = vector.as_ptr();
-    let out_len: usize = vector.len();
+    //
+    // Encodes `String`s into UTF-16 and then marks them for manual drop
+    //
+    // A vector of `usize`s,
+    // where every element with an even index is an array pointer
+    // and every element with an odd index is a length of the array of the previous pointer
+    let pointers: ManuallyDrop<Vec<usize>> = ManuallyDrop::new(renders
+        .into_iter()
+        .flat_map(|render: [String; 1]| {
+            let manual: ManuallyDrop<Vec<u16>> =
+                ManuallyDrop::new(render[0].encode_utf16().collect());
+            let ptr: *const u16 = manual.as_ptr();
+            let length: usize = manual.len();
+            [ptr as usize, length]
+        })
+        .collect());
+    let out_ptr: *const usize = pointers.as_ptr();
+    let out_len: usize = pointers.len();
     // vector.shrink_to_fit();
 
     Box::new([out_ptr as usize, out_len])
