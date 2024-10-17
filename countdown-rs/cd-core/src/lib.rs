b941732fa8fc4ced1a0ee66d7985ce7e3b74275d
SomeBodY1CE <someonecallsyou@gmail.com>
2024-10-17 18:59:52 +0300

    refactor(ffi): initializing in js

--- countdown-rs/cd-core/src/lib.rs
+++ countdown-rs/cd-core/src/lib.rs
@@ -4,6 +4,13 @@ Copyright 2024 sby1ce
 SPDX-License-Identifier: AGPL-3.0-or-later
 */
 
+const W_DIVISOR: i64 = 1000 * 60 * 60 * 24 * 7;
+const D_DIVISOR: i64 = 1000 * 60 * 60 * 24;
+const H_DIVISOR: i64 = 1000 * 60 * 60;
+const M_DIVISOR: i64 = 1000 * 60;
+const S_DIVISOR: i64 = 1000;
+const MS_DIVISOR: i64 = 1;
+
 #[derive(Clone, Copy)]
 pub struct TimeUnit {
     pub suffix: &'static str,
@@ -17,12 +24,42 @@ impl TimeUnit {
 }
 
 const TIME_UNITS: [TimeUnit; 6] = [
-    TimeUnit::new("w", 1000 * 60 * 60 * 24 * 7),
-    TimeUnit::new("d", 1000 * 60 * 60 * 24),
-    TimeUnit::new("h", 1000 * 60 * 60),
-    TimeUnit::new("m", 1000 * 60),
-    TimeUnit::new("s", 1000),
-    TimeUnit::new("ms", 1),
+    TimeUnit::new("w", W_DIVISOR),
+    TimeUnit::new("d", D_DIVISOR),
+    TimeUnit::new("h", H_DIVISOR),
+    TimeUnit::new("m", M_DIVISOR),
+    TimeUnit::new("s", S_DIVISOR),
+    TimeUnit::new("ms", MS_DIVISOR),
+];
+
+const MINUS_CHAR: u16 = 45;
+const SPACE_CHAR: u16 = 32;
+const W_SUFFIX: &[u16] = &[119];
+const D_SUFFIX: &[u16] = &[100];
+const H_SUFFIX: &[u16] = &[104];
+const M_SUFFIX: &[u16] = &[109];
+const S_SUFFIX: &[u16] = &[115];
+const MS_SUFFIX: &[u16] = &[109, 115];
+
+#[derive(Clone, Copy)]
+pub struct TimeUnitU16 {
+    pub suffix: &'static [u16],
+    pub divisor: i64,
+}
+
+impl TimeUnitU16 {
+    const fn new(suffix: &'static [u16], divisor: i64) -> Self {
+        Self { suffix, divisor }
+    }
+}
+
+const TIME_UNITS_U16: [TimeUnitU16; 6] = [
+    TimeUnitU16::new(W_SUFFIX, W_DIVISOR),
+    TimeUnitU16::new(D_SUFFIX, D_DIVISOR),
+    TimeUnitU16::new(H_SUFFIX, H_DIVISOR),
+    TimeUnitU16::new(M_SUFFIX, M_DIVISOR),
+    TimeUnitU16::new(S_SUFFIX, S_DIVISOR),
+    TimeUnitU16::new(MS_SUFFIX, MS_DIVISOR),
 ];
 
 pub enum FormatOption {
@@ -58,6 +95,61 @@ impl FormatOption {
             Self::Millisecond => TIME_UNITS[5],
         }
     }
+}
+
+fn calculate_interval(interval: i64, divisor: i64) -> (i64, String) {
+    let new_interval: i64 = interval.checked_rem_euclid(divisor).unwrap_or_default();
+    let unit_count: i64 = interval.checked_div_euclid(divisor).unwrap_or_default();
+    (new_interval, unit_count.to_string())
+}
+
+pub trait Accumulate<T: Clone> {
+    const MINUS_SIGN: T;
+    const SPACE_SIGN: T;
+    fn accumulate(self, interval: i64, format_option: &FormatOption) -> (i64, Self);
+    fn plus(self, element: T) -> Self;
+}
+
+struct MyStr {
+    inner: String,
+}
+
+impl MyStr {
+    fn into_string(self) -> String {
+        self.inner
+    }
+}
+
+impl Default for MyStr {
+    fn default() -> Self {
+        Self {
+            // Some calculating concluded that with the length 20
+            // the possibility of reallocation is close to 0
+            inner: String::with_capacity(20),
+        }
+    }
+}
+
+impl Accumulate<&str> for MyStr {
+    const MINUS_SIGN: &'static str = "-";
+    const SPACE_SIGN: &'static str = " ";
+    fn accumulate(self, interval: i64, format_option: &FormatOption) -> (i64, Self) {
+        let time_unit: TimeUnit = format_option.as_time_unit();
+        let (new_interval, unit_count) = calculate_interval(interval, time_unit.divisor);
+        let new_accumulator: Self = Self {
+            inner: self.inner + &unit_count + time_unit.suffix + Self::SPACE_SIGN,
+        };
+        (new_interval, new_accumulator)
+    }
+    fn plus(self, element: &str) -> Self {
+        Self {
+            inner: self.inner + element,
+        }
+    }
+}
+
+fn next(format_options: &[FormatOption]) -> &[FormatOption] {
+    &format_options[1..format_options.len()]
 }
 
 fn calculate_interval(interval: i64, divisor: i64) -> (i64, String) {
@@ -174,6 +266,73 @@ pub fn update_timers_(now: i64, origins: Vec<i64>) -> Vec<[String; 1]> {
         .collect::<Vec<[String; 1]>>()
 }
 
+/// Works with positive intervals
+fn reduce_interval_u16(
+    interval: i64,
+    accumulator: Vec<u16>,
+    format_options: &[FormatOption],
+) -> Vec<u16> {
+    if format_options.is_empty() {
+        return accumulator;
+    }
+    let format_option: &FormatOption = &format_options[0];
+    let time_unit: TimeUnitU16 = format_option.to_time_unit_u16();
+    let (new_interval, unit_count) = calculate_interval(interval, time_unit.divisor);
+    let new_accumulator: Vec<u16> = vec_plus(
+        vec_extend(
+            vec_extend(
+                accumulator,
+                &unit_count.to_string().encode_utf16().collect::<Vec<u16>>(),
+            ),
+            time_unit.suffix,
+        ),
+        SPACE_CHAR,
+    );
+
+    reduce_interval_u16(new_interval, new_accumulator, next(format_options))
+}
+
+fn vec_plus<T>(mut accumulator: Vec<T>, other: T) -> Vec<T> {
+    accumulator.push(other);
+    accumulator
+}
+
+fn vec_extend<T: Clone>(mut accumulator: Vec<T>, other: &[T]) -> Vec<T> {
+    if accumulator.len() + other.len() > accumulator.capacity() {
+        // Basically panicing because we can't safely reallocate accumulator
+        eprintln!(
+            "accumulator + other {} exceeded capacity {}",
+            accumulator.len() + other.len(),
+            accumulator.capacity()
+        );
+        accumulator.clear();
+    } else {
+        accumulator.extend_from_slice(other);
+    }
+    accumulator
+}
+
+#[must_use]
+pub fn update_u16(accumulator: Vec<u16>, now: i64, origin: i64) -> Vec<u16> {
+    let format_options: [FormatOption; 4] = [
+        FormatOption::Day,
+        FormatOption::Hour,
+        FormatOption::Minute,
+        FormatOption::Second,
+    ];
+
+    let interval: i64 = origin - now;
+    let abs_interval: i64 = interval.abs();
+
+    let accumulator: Vec<u16> = if interval < 0 {
+        vec_plus(accumulator, MINUS_CHAR)
+    } else {
+        accumulator
+    };
+
+    reduce_interval_u16(abs_interval, accumulator, &format_options)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -181,7 +340,7 @@ mod test {
     #[test]
     fn test() {
         let origins: Vec<i64> = vec![0];
-        let a: Vec<[String; 1]> = update_timers_(100_000, origins);
+        let a: Vec<[String; 1]> = update_timers(100_000, origins);
 
         assert_eq!(a, vec![["-0d 0h 1m 40s "]]);
     }
